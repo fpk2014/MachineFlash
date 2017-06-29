7975a9c092fec2ba47e93260d5dc31e0b1aaa87b
fpk2014 <994347033@qq.com>
2017-06-29 16:46:46 +0800

    重写js

--- MachineFlash.user.js
+++ MachineFlash.user.js
@@ -1,23 +1,33 @@
 // ==UserScript==
 // @name         MachineFlash
-// @namespace    http://www.forsu.cn/
-// @version      0.3
-// @description  try to take over the world!
+// @namespace    https://github.com/fpk2014
+// @version      0.4
+// @description  激切抢预约，自用脚本
 // @author       fpk2014
 // @match        http://210.39.2.59:8081/web.equipmentBooking/web/book*
 // @updateURL    https://raw.githubusercontent.com/fpk2014/MachineFlash/master/MachineFlash.user.js
 // @downloadURL  https://raw.githubusercontent.com/fpk2014/MachineFlash/master/MachineFlash.user.js
-// @require  http://crypto.stanford.edu/sjcl/sjcl.js
-// @grant    GM_getValue
-// @grant    GM_setValue
-// @grant    GM_registerMenuCommand
+// @require      http://crypto.stanford.edu/sjcl/sjcl.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
-var encKey  = GM_getValue ("encKey",  "");
-var yourJob     = GM_getValue ("lognUsr", "");
-var yourID   = GM_getValue ("lognPwd", "");
 
-if ( ! encKey) {
+var myurl = window.location.href;
+var FLASH_TIME = 500;                      //刷新网页时间，单位：ms
+var WAIT_LOAD_TIME = 200;                  //等待预约界面加载完全，如果延迟过大，必须调整该值，单位：ms
+var Find_ONE_USEFUL_MACHINE_TIME = 500;    //切换到下一台机器的时间，单位：ms
+var MACHINE_NUMBER = 2;                    //机器数量
+var IDEAL_TIME = 10;                       //默认选择的时间段
+var PRINT_LOG = false;
+var encKey  = GM_getValue ("encKey",  "");
+var yourJob = GM_getValue ("lognUsr", "");
+var yourID  = GM_getValue ("lognPwd", "");
+
+
+//-- Prepare: Get_Value --//
+if (!encKey) {
     encKey  = prompt (
         'Script key not set for ' + location.hostname + '. Please enter a random string:',
         ''
@@ -25,9 +35,9 @@ if ( ! encKey) {
     GM_setValue ("encKey", encKey);
     yourJob     = yourID = "";   // New key makes prev stored values (if any) unable to decode.
 }
-yourJob         = decodeOrPrompt (yourJob,   "作业名称", "lognUsr");
-yourID       = decodeOrPrompt (yourID, "手机号码", "lognPwd");
-console.log("作业名称:"+yourJob+"  手机号码:"+yourID);
+GM_registerMenuCommand ("修改作业名称", changeUsername);
+GM_registerMenuCommand ("修改手机号码", changePassword);
+
 function decodeOrPrompt (targVar, userPrompt, setValVarName) {
     if (targVar) {
         targVar     = unStoreAndDecrypt (targVar);
@@ -51,9 +61,6 @@ function unStoreAndDecrypt (jsonObj) {
 }
 
 //-- Add menu commands that will allow U and P to be changed.
-GM_registerMenuCommand ("修改作业名称", changeUsername);
-GM_registerMenuCommand ("修改手机号码", changePassword);
-
 function changeUsername () {
     promptAndChangeStoredValue (yourJob,   "作业名称", "lognUsr");
 }
@@ -71,66 +78,64 @@ function promptAndChangeStoredValue (targVar, userPrompt, setValVarName) {
 }
 
 
-var myurl = window.location.href;
-var flashTime = 500;                  //刷新网页时间，单位：ms
-var waitLoadTime = 200;                //等待预约界面加载完全，如果延迟过大，必须调整该值，单位：ms
-var findOneUsefulMachineTime = 500;    //切换到下一台机器的时间，单位：ms
-var machineNum = 2;                    //机器数量
-var idealtime = 10;                    //默认选择的时间段
+yourJob = decodeOrPrompt (yourJob,   "作业名称",  "lognUsr");
+yourID  = decodeOrPrompt (yourID,    "手机号码",  "lognPwd");
 
-function print(name){
-    console.log(name);
+Print("作业名称:" + yourJob + "  手机号码:" + yourID);
+
+
+//- - Start - -//
+//创建暂停按钮
+var button = document.createElement("button");
+button.innerHTML = "Stop";
+button.id="loopButton";
+button.setAttribute("style", "position:absolute; right: 0px;font-size: 45px; font-style: italic; color:#ff0000;background-color:#080808;height:95px;font-family:Microsoft YaHei;");
+var body = document.getElementsByTagName("div")[0];
+body.appendChild(button);
+button.addEventListener ("click", function() {
+    if(button.innerHTML==="Start"){
+        window.location.reload();
+    }
+    if(button.innerHTML==="Stop"){
+        button.innerHTML="Start";
+    }
+});
+
+
+function Print(name){
+    if(PRINT_LOG)
+        console.log(name);
 }
 
-function sleep(ms) {
+function Sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 //window.alert = function(str){ return ;}; //禁止alert
 
-//模拟鼠标点击
-/*
-function clickChoose(el){
-    var event = document.createEvent('MouseEvent');
-    //event.initEvent('click', false, false);
-    //el.dispatchEvent(event);
-    
-    event.initMouseEvent("click", true, true, window,
-                         0, 0, 0, 0, 0,
-                         false, false, false, false,
-                         0, null);
-                         
-    //event.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    el.dispatchEvent(event);
-}
-*/
 
-
-function selectBase(reg){
-    return document.evaluate(
+function Get_Right_Item(reg, attributeName, indexName){
+    var items = document.evaluate(
         reg,
         document,
         null,
         XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
         null);
-}
-
-
-function getRightItem(items, attributeName, indexName){
-    for(var i=0; i<items.snapshotLength; i++){
+    var i=0;
+    //alert(aUseful.snapshotLength);
+    while(i<items.snapshotLength){
         var  thisItem = items.snapshotItem(i);
         if (thisItem.getAttribute(attributeName).indexOf(indexName) != -1) {
-            //print("items ok");
+            //Print("items ok");
             return thisItem;
-        }else{
-            //print("false");
         }
+        i++;
     }
     return false;
 }
 
 //用于选择到激切最近开放的工作日
-function getFinalItem(items, attributeName){
+function Get_Final_Item(items, attributeName){
     var i=items.snapshotLength;
     if(i !== 0){
         return items.snapshotItem(i-1);
@@ -139,71 +144,33 @@ function getFinalItem(items, attributeName){
     }
 }
 
-/*
-//测试用的
-function ShowObjProperty(Obj) {
-    var PropertyList='';
-    var PropertyCount=0;
-    var i;
-    for(i in Obj){
-        if(Obj.i !==null)
-            PropertyList=PropertyList+i+'属性：'+Obj.i+'\r\n';
-        else
-            PropertyList=PropertyList+i+'方法\r\n';
-    }
-    alert(PropertyList);
-}
 
-function showAttr(obj){
-    var str='';
-    for(var tmp in obj){
-        str=str+tmp+", ";
-    }
-    alert(str);
-}
-*/
-
-function closeConfirm(){
-    var allChoose = selectBase('//a[@class]');
-    var thisChoose = getRightItem(allChoose, "class", "order_popup_close").click();
-}
-
-//勾选确认框，跳转到下一步骤
-function enterProcess(){
-    var allChoose = selectBase('//a[@class]');
-    var allCheck = selectBase('//input[@id]');
-    var allClick = selectBase('//a[@id]');
-    var thisChoose = getRightItem(allChoose, "class", "dispark_order chke").click();
-
-    var thisCheck = getRightItem(allCheck, "id", "needNow").click();//勾选同意
-
-    var thisClick = getRightItem(allClick, "id", "equipmentListPage").click();
-    closeConfirm();
-}
-
-//选中最后一天
-function clickFinalDay(){
-    var allDay = selectBase('//li[@onclick]');
-    var thisDay = getFinalItem(allDay, "onclick").click();
+function Click_Final_Day(reg, attributeName){
+    var items = document.evaluate(
+        reg,
+        document,
+        null,
+        XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+        null);
+    return Get_Final_Item(items, attributeName);
     //showAttr(thisDay);
     //alert("ClickDay OK");
 }
 
+
 //填入作业名称或手机号
-function inputNeed(name, value){
-    var allMachine = selectBase('//input[@id]');
-    var _my= getRightItem(allMachine, "id", name);
+function Input_Name_Or_PhoneID(reg, attributeName, indexName, value_reg, value){
+    var _my= Get_Right_Item(reg, attributeName, indexName);
     if (_my === 0){
-        //print("get machine false");
+        //Print("get machine false");
         window.location.reload();
     }else{
-        //showAttr(_my);
-        //alert(_my.value);
-        _my.setAttribute("value", value);
+        _my.setAttribute(value_reg, value);
     }
 }
 
-function clickTime(time){
+//选择特定的时间段
+function Click_Time(reg, attributeName, indexName, time_reg, time){
     var realTime;
     switch(time){
         case 9:
@@ -227,18 +194,23 @@ function clickTime(time){
         case 20:
             realTime = "114";break;
     }
-    var aUseful = selectBase('//a[@class]');
+    var aUseful = document.evaluate(
+        reg,
+        document,
+        null,
+        XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+        null);;
 
     var i=0;
     //alert(aUseful.snapshotLength);
     while(i<aUseful.snapshotLength){
         var thisAUseful = aUseful.snapshotItem(i);
-        if (thisAUseful.getAttribute("class").indexOf("no_subscribe") != -1) {
+        if (thisAUseful.getAttribute(attributeName).indexOf(indexName) != -1) {
             //alert(thisAUseful.getAttribute("name"));
-            //print("items ok");
-            if(thisAUseful.getAttribute("name")===realTime){
+            //Print("items ok");
+            if(thisAUseful.getAttribute(time_reg)===realTime){
                 //alert("OK");
-                print(time+" clocks is OK!!!Choose!!!");
+                Print(time+" clocks is OK!!!Choose!!!");
                 //clickChoose(thisAUseful);
                 thisAUseful.click();
                 return true;
@@ -247,14 +219,14 @@ function clickTime(time){
         i++;
     }
 
-    print(time+" clocks can't been preserved;");
+    Print(time+" clocks can't been preserved;");
     return false;
 }
 
 
 
 //选中某台机器
-function clickMachine(num){
+function Click_Machine(reg, attributeName, num){
     var realNum;
     switch(num){
         case 2:
@@ -270,15 +242,12 @@ function clickMachine(num){
             realNum="192";
             break;
     }
-    var allMachine = selectBase('//a[@name]');
-    var thisMachine = getRightItem(allMachine, "name", realNum);
+    var thisMachine = Get_Right_Item(reg, attributeName, realNum);
     //clickChoose(thisMachine);
     if(thisMachine!==false){
-        //print("thisMachine OK");
+        //Print("thisMachine OK");
         //clickChoose(thisMachine);
-        thisMachine.click();
-        //clickMachine(9);
-        return true;
+        return thisMachine;
     }else{
         return false;
     }
@@ -286,105 +255,91 @@ function clickMachine(num){
 
 
 //遍历某机器的可用时间段
-function findUseful(){
-    //print("run findUseful");
-    var allUseful = selectBase('//a[@class]');
+function Find_Useful(reg, attributeName, indexName){
+    //Print("run Find_Useful");
+    var allUseful = document.evaluate(
+        reg,
+        document,
+        null,
+        XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+        null);
     var i=0;
     //alert(allUseful.snapshotLength);
     while(i<allUseful.snapshotLength){
         var thisUseful = allUseful.snapshotItem(i);
         //alert(thisUseful.getAttribute("class"));
-        if (thisUseful.getAttribute("class").indexOf("no_subscribe") != -1) {
-            //alert("findUseful true");
-            print("found useful!!!Choose!!!");
+        if (thisUseful.getAttribute(attributeName).indexOf(indexName) != -1) {
+            //alert("Find_Useful true");
+            Print("found useful!!!Choose!!!");
             //clickChoose(thisUseful);
             thisUseful.click();
             return true;
         }
         i++;
     }
-    print("All machine have been preserved;");
+    Print("All machine have been preserved;");
     return false;
 }
-//创建暂停按钮
-var button = document.createElement("button");
-button.innerHTML = "Stop";
-button.id="loopButton";
-button.setAttribute("style", "position:absolute; right: 0px;font-size: 45px; font-style: italic; color:#ff0000;background-color:#080808;height:95px;font-family:Microsoft YaHei;");
-var body = document.getElementsByTagName("div")[0];
-body.appendChild(button);
-button.addEventListener ("click", function() {
-    if(button.innerHTML==="Start"){
-        window.location.reload();
-    }
-    if(button.innerHTML==="Stop"){
-        button.innerHTML="Start";
-    }
-});
 
-function myTest(){
-    //alert("run");
-    var f = document.getElementsByClassName("subscribe");
-    alert(f.length);
-    var i=0;
-    while(i<f.length){
-        alert(f[i].innerHTML);
-    }
-}
 
-async function allProcess() {
+async function All_Process() {
     try{
 
-        //if( getRightItem(selectBase('//input[@id]'), "id", "jobName")!==null)
-        //准备工作
-        enterProcess();
-        inputNeed("jobName", yourJob);
-        inputNeed("bookingUserTel", yourID);
+        //if( Get_Right_Item(selectBase('//input[@id]'), "id", "jobName")!==null)
+        //准备工作:跳转到下一步骤
+        Get_Right_Item('//a[@class]',   "class",  "dispark_order chke").click();                        //点击预约按钮
+        Get_Right_Item('//input[@id]',  "id",     "needNow"           ).click();                        //勾选同意
+        Get_Right_Item('//a[@id]',      "id",     "equipmentListPage" ).click();                        //点击下一步按钮
+        Get_Right_Item('//a[@class]',   "class",  "order_popup_close" ).click();                        //关闭协议框
+        
+        Input_Name_Or_PhoneID('//input[@id]',  "id",  "jobName",        "value", yourJob);              //输入作业名称
+        Input_Name_Or_PhoneID('//input[@id]',  "id",  "bookingUserTel", "value", yourID );              //输入手机号码
 
         //
-        await sleep(waitLoadTime);  //等待加载完成
+        await Sleep(WAIT_LOAD_TIME);    //等待加载完成
 
-        //开始找可以预约的，遍历所有机器
-        for(var i=1;i<=machineNum;i++){
-            await sleep(findOneUsefulMachineTime*i); //按顺序执行
-            print("test machine "+i);
-            clickMachine(i);  //选择几号机器
-            clickFinalDay();  //选择最后一天
+        //- - 开始找可以预约的 - -//
+        //遍历所有机器
+        for(var i=1; i<=MACHINE_NUMBER; i++){
+            await Sleep(Find_ONE_USEFUL_MACHINE_TIME*i); //等待加载完成
+            
+            Print("test machine "+i);
+            Click_Machine('//a[@name]',       "name", i).click();                                         //选择i号机器
+            Click_Final_Day('//li[@onclick]', "onclick").click();                                         //选择最后一天
+            
             //遍历所有可用的时间段
-            if(button.innerHTML==="Start"){
-                return;
-            }
-
-            await sleep(waitLoadTime);  //等待加载完成
-            //if(clickTime(idealtime)===true){return;}  //寻找特点时间点的机器
-            if(findUseful()===true){return;}
+            await Sleep(WAIT_LOAD_TIME);                                                                  //等待加载完成
+            if(button.innerHTML==="Start"){return;}
+            //if(Click_Time('//a[@class]',   "class", "no_subscribe", "name", IDEAL_TIME===true)){return;}  //寻找特定时间点的机器
+            if(Find_Useful('//a[@class]',  "class", "no_subscribe")===true){return;}                      //寻找可用的机器
         }
 
-        //找不到可用的机器，重新加载
-        await sleep(flashTime);
+        //- - 找不到可用的机器，重新加载 - -//
+        await Sleep(FLASH_TIME);
         window.location.reload();
     }
     catch(err){
-        print(err);
-        //await sleep(10000);
+        Print(err);
+        //await Sleep(10000);
         window.location.reload();
     }
 }
 
 
+//运行
+All_Process();
 
-allProcess();
-//if(allProcess()===false){window.location.reload();}
-//如果是某div的滚动条如id=content的div，类似：
-//document.body.scrollTop = document.body.scrollHeight;
-//document.getElementById('content').scrollTop = document.getElementById('content').scrollHeight;
+async function test(){
+    Get_Right_Item(       '//a[@class]',   "class",  "dispark_order chke").click();
+    Get_Right_Item(       '//input[@id]',  "id",     "needNow").click();//勾选同意
+    Get_Right_Item(       '//a[@id]',      "id",     "equipmentListPage").click();
+    Get_Right_Item(       '//a[@class]',   "class",  "order_popup_close").click();
+    Input_Name_Or_PhoneID('//input[@id]',  "id",     "jobName",        "value", yourJob);
+    Input_Name_Or_PhoneID('//input[@id]',  "id",      "bookingUserTel", "value", yourID);
 
-function test(){
-    enterProcess();
-    inputNeed("jobName", yourJob);
-    inputNeed("bookingUserTel", yourID);
-
-    clickMachine(machineNum);  //选择几号机器
-    clickFinalDay();  //选择最后一天
+    await Sleep(1000);
+    Click_Machine('//a[@name]',       "name", MACHINE_NUMBER).click();  //选择几号机器
+    Click_Final_Day('//li[@onclick]', "onclick").click(); //选择最后一天
 }
 
+//test();
